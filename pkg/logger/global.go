0eb319335f77c93baa9195d79469c68f69778ce2
zakaria <52750533+zcubbs@users.noreply.github.com>
2023-06-24 17:33:53 +0200

    :sparkles: add logger setup

--- pkg/logger/global.go
+++ pkg/logger/global.go
@@ -1,6 +1,10 @@
 package logger
 
 import (
+	"github.com/sirupsen/logrus"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
 	"sync"
 )
 
@@ -26,4 +30,57 @@ func L() Logger {
 	l := _globalL
 	_globalMU.RUnlock()
 	return l
+}
+
+type Type string
+
+const (
+	LogrusLogger Type = "logrus"
+	ZapLogger    Type = "zap"
+)
+
+func SetupLogger(loggerType Type) {
+	if loggerType == LogrusLogger {
+		setupLogrus()
+		return
+	}
+	if loggerType == ZapLogger {
+		setupZap()
+		return
+	}
+
+	panic("invalid logger type")
+}
+
+func setupLogrus() {
+	logrusLog := logrus.New()
+	logrusLog.SetReportCaller(false)
+	logrusLog.SetFormatter(&logrus.JSONFormatter{})
+	logrusLog.SetOutput(os.Stdout)
+	logrusLog.SetLevel(logrus.InfoLevel)
+	log, _ := NewLogrusLogger(logrusLog)
+	ReplaceGlobals(log)
+}
+
+func setupZap() {
+	consoleEncoder := zapcore.NewJSONEncoder(zapcore.EncoderConfig{
+		TimeKey:        "ts",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		FunctionKey:    zapcore.OmitKey,
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.EpochTimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	})
+	core := zapcore.NewCore(consoleEncoder,
+		zapcore.Lock(zapcore.AddSync(os.Stderr)),
+		zapcore.DebugLevel)
+	zapLogger := zap.New(core)
+	log, _ := NewZapLogger(zapLogger)
+	ReplaceGlobals(log)
 }
